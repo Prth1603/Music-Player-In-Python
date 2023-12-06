1d66e05799f5f9b25ccbbc35a5344ea2fb6eac33
Prth1603 <90952304+Prth1603@users.noreply.github.com>
2023-12-06 11:46:06 +0530

    Create music_player.py

---
+++ music_player.py
@@ -0,0 +1,11 @@
+#music player in python
+from tkinter import *
+import pygame
+import time
+from tkinter import filedialog
+import random
+import os
+from tkinter import ttk
+from mutagen.mp3 import MP3
+
+root = TK()
